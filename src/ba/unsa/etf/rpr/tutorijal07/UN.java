3188d093d9c7205dcda24fcf702b3ec2d1a3b5b3
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-03 22:13:44 +0100

    Napravljena klasa UN sa jednim atributom, ArrayList<Drzava> listadrzava. Kreiran i konstruktor bez parametara i getteri i setteri kako bi klasa zadovoljavala JavaBeans specifikaciju

---
+++ src/ba/unsa/etf/rpr/tutorijal07/UN.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.tutorijal07;
+
+import java.util.ArrayList;
+
+public class UN {
+    private ArrayList<Drzava> listadrzava;
+
+    public UN() {
+        this.listadrzava=new ArrayList<Drzava>();
+    }
+
+    public ArrayList<Drzava> getListadrzava() {
+        return listadrzava;
+    }
+
+    public void setListadrzava(ArrayList<Drzava> listadrzava) {
+        this.listadrzava = listadrzava;
+    }
+
+}
