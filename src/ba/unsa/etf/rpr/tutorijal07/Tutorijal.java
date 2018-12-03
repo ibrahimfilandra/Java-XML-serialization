1329604530acee77083ba1543fcaaafd6ac37476
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-03 23:25:06 +0100

    zapocet rad na funkciji ucitajGradove(). Kreiran novi scanner koji ucitava datoteku gradovi.txt

--- src/ba/unsa/etf/rpr/tutorijal07/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal07/Tutorijal.java
@@ -1,6 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal07;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Tutorijal {
+
+    void ucitajGradove() {
+
+        Scanner ulaz;
+
+        try {
+             ulaz=new Scanner(new FileReader("gradovi.txt"));
+        }
+catch(FileNotFoundException e) {
+    System.out.println("Datoteka gradovi.txt ne postoji ili se ne može otvoriti.");
+    System.out.println("Greška: " + e);
+    return;
+}
+
+
+    }
 
     public static void main (String []args){
 
