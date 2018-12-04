baca4e5af746b2e3fd475e4d51264b3c0f1b2bf7
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-04 01:20:50 +0100

    zavrsena metoda ucitajGradove. Svaka linija koju procita scanner ulaz, koji cita datoteku gradovi.txt, se pohranjuje u string. Nad tim stringom se poziva funkcija split sa znakom zareza kao argumentom, string.split(","). Funkcija split vraca niz stringova koji su se nalazili izmedju znakova zareza, odakle se lako dobijaju podaci o nazivu grada i izmjerenim temperaturama

--- src/ba/unsa/etf/rpr/tutorijal07/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal07/Tutorijal.java
@@ -2,11 +2,13 @@ package ba.unsa.etf.rpr.tutorijal07;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tutorijal {
 
-    void ucitajGradove() {
+    static ArrayList<Grad> ucitajGradove() {
 
         Scanner ulaz;
 
@@ -16,14 +18,35 @@ public class Tutorijal {
 catch(FileNotFoundException e) {
     System.out.println("Datoteka gradovi.txt ne postoji ili se ne može otvoriti.");
     System.out.println("Greška: " + e);
-    return;
+    return null;
 }
 
-
+ArrayList<Grad> gradovi = new ArrayList<Grad>();
+        int i=0;
+while(ulaz.hasNext()) {
+    String pomocni= ulaz.next();
+    gradovi.add(new Grad());
+String [] elementi=pomocni.split(",");
+gradovi.get(i).setNaziv(elementi[0]);
+int j=0;
+double [] temperature = new double[1000];
+while(true) {
+    try{
+        temperature[j]=Double.parseDouble(elementi[j+1]);
     }
+    catch(ArrayIndexOutOfBoundsException e) {
+        break;
+    }
+    j++;
+}
+gradovi.get(i).setTemperature(temperature);
+i++;
+}
+
+return gradovi;
+}
 
     public static void main (String []args){
-
 
     }
 
